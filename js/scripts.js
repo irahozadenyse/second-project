6db32dfebdfe6afbf2798c14b2352828c8733634
irahozadenyse <irahodeny1@gmail.com>
2019-07-20 19:44:38 +0200

    added css

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,58 @@
 
 function myApp(){
 
+var ff= document.getElementById("fi").value;
+var ll= document.getElementById("la").value;
+var cc=document.getElementById("ce").value;
+var yy =document.getElementById("ye").value;
+var mm = document.getElementById("mo").value;
+var dd =document.getElementById("da").value; 
 
-var yy =document.getElementById("ye").Value;
+ var gende=document.getElementById("ma", "fe").value;
  
- var calcCc= (yy-1)/100;
- var nad=calcCc.toFixed(0);
+var calcDay = (((cc/4) -2*cc-1 )+ ((5*yy/4 )) + ((26*(mm+1)/10)) + dd )%7
+var num= calcDay.toFixed(0);
 
-var cc=document.getElementById("ce").Value;
-
-var mm = document.getElementById("mo").Value;
-
-var dd =document.getElementById("da").Value;
- var ff= document.getElementById("fi").Value;
- var ll= document.getElementById("la").Value;
- 
- var gender=prompt("Enter the gender:");
- 
-var calcDay = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7;
-var n= calcDay.toFixed(0);
-var num=n;
 var names=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",
 "Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-//var number =[0,1,2,3,4,5,6]
-if (num== 0 && gender=="male") {
+
+if (num== 0 && gende=="male") {
 document.getElementById("get").innerHTML=names[0];
 }
-else if (num ==1  && gender=="male") {
+else if (num ==1  && gende== "male") {
 document.getElementById("get").innerHTML =names[1];
-}else if (num == 2 && gender=="male") {
+}else if (num == 2 && gende == "male") {
 document.getElementById("get").innerHTML= names[2];
-}else if (num == 3 && gender=="male") {
+}else if (num == 3 && gende=="male") {
 document.getElementById("get").innerHTML=names[3];
-}else if (num == 4 && gender=="male") {
+}else if (num == 4 && gende=="male") {
 document.getElementById("get").innerHTML=names[4];
-}else if (num == 5 && gender=="male") {
+}else if (num == 5 && gende=="male") {
 document.getElementById("get").innerHTML=names[5];
-}else if (num == 6 && gender=="male") {
+}else if (num == 6 && gende=="male") {
 document.getElementById("get").innerHTML=names[6];
 }
-if (num== 0 && gender=="female") {
+
+
+else if (num== 0 && gende=="female") {
 document.getElementById("get").innerHTML=names[7];
 }
-else if (num == 1 && gender=="female") {
+else if (num == 1 && gende=="female") {
 document.getElementById("get").innerHTML=names[8];
-}else if(num == 2 && gender=="female") {
+}else if(num == 2 && gende=="female") {
 document.getElementById("get").innerHTML=names[9];
-}else if (num == 3 && gender=="female") {
+}else if (num == 3 && gende=="female") {
 document.getElementById("get").innerHTML=names[10];
-}else if (num == 4 && gender=="female") {
+}else if (num == 4 && gende=="female") {
 document.getElementById("get").innerHTML=names[11];
-}else if (num == 5 && gender=="female") {
+}else if (num == 5 && gende=="female") {
 document.getElementById("get").innerHTML=names[12];
-}else if (num == 6 && gender=="female") {
+}else if (num == 6 && gende=="female") {
 document.getElementById("get").innerHTML=names[13];
 }
+return names;
 }
+
 
 
 
